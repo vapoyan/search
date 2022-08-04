ccc2b38b392db8f7cfac405e1b3aba1f15b4c891
Viktor Apoyan <chamich.apps@gmail.com>
2022-08-04 22:12:55 +0200

    Initial commit

---
+++ sample/src/main/kotlin/app/coinfo/sample/search/SampleActivity.kt
@@ -0,0 +1,11 @@
+package app.coinfo.sample.search
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SampleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sample)
+    }
+}
