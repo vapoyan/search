e02deb0032c118c92fdd98ec31473a90fb68e696
Viktor Apoyan <chamich.apps@gmail.com>
2022-08-24 23:12:43 +0200

    Unused res and classes removed.

--- search/src/main/kotlin/app/coinfo/feature/search/presentation/feature/search/SearchState.kt
+++
@@ -1,9 +0,0 @@
-package app.coinfo.feature.search.presentation.feature.search
-
-import app.coinfo.feature.search.domain.model.SearchResult
-
-internal data class SearchState(
-    val isLoading: Boolean = false,
-    val result: SearchResult = SearchResult(emptyList()),
-    val error: String = ""
-)
